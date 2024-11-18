a515ef23f5b221002e0ea8119f37ceca34654f3c
latifaChakir <chakirlatifa2001@gmail.com>
2024-11-18 23:12:37 +0100

    add branch to added all files needed

--- src/main/java/com/example/citronix/domain/mapper/FarmMapper.java
+++ src/main/java/com/example/citronix/domain/mapper/FarmMapper.java
@@ -1,4 +1,7 @@
 package com.example.citronix.domain.mapper;
 
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface FarmMapper {
 }
--- src/main/java/com/example/citronix/domain/mapper/FieldMapper.java
+++ src/main/java/com/example/citronix/domain/mapper/FieldMapper.java
@@ -1,4 +1,8 @@
 package com.example.citronix.domain.mapper;
 
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+
 public interface FieldMapper {
 }
